fa601e513a479f6fe6ee800e1cc0184997d8c188
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-08-03 12:51:40 -0300

    feat: update Application
    Rename file

--- src/main/java/com/wesleybertipaglia/securepass/Application.java
+++ src/main/java/com/wesleybertipaglia/securepass/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurepassApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurepassApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
